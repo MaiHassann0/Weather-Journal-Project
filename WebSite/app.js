aea545d894bf6af5512ea9b2cd78db50350e6500
MaiiHassan <mai7assan99@gmail.com>
2023-01-19 10:19:19 +0200

    commit

--- WebSite/app.js
+++ WebSite/app.js
@@ -20,7 +20,7 @@ function func(e){
   .then(function(data){
     console.log(data);
     //adding data to POST route
-    postData('/post-data', {date:d, temp:data.list[0].main.temp, content:feelings})
+    postData('/post-data', {newDate:d, temp:data.list[0].main.temp, content:feelings})
     updateUi();
   })
 };
@@ -39,5 +39,39 @@ const getWeatherData = async (url, zipCode, apiKey)=>{
 
 //chain another Promise that updates the UI dynamically 
 // another async function that is called after the completed POST request
+/* POST data */
+const postData = async (url= '', data={})=>{
+  console.log(data);
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers:{
+      "Content-Type": "application/json",
+    },
 
+    body: JSON.stringify(data) //create json stringify
+  });
+  //try catch errors
+  try{
+    const newData = await response.json();
+    return newData;
+  }catch(error){
+    console.log('error', error);
+  }
+}
+
+/* GET data */
+const updateUI = async()=>{
+  const req = await fetch('/get-data');
+  //try catch errors
+  try{
+    const AllDataa = await req.json();
+    document.getElementById('date').innerHTML= `Date: ${AllDataa[0].date}`;
+    document.getElementById('temp').innerHTML= `Temperature: ${AllDataa[0].temp}`;
+    document.getElementById('content').innerHTML= `Feelings: ${AllDataa[0].content}`;
+
+  }catch(error){
+    console.log('error', error);
+  }
+}
 
